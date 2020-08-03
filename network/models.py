53111f3a07e6fd3e5946965559771c7528e4e858
Maxwell DeMaio <maxdman321@gmail.com>
2020-08-03 18:57:01 -0400

    Updated gitignore and started All Posts route

--- network/models.py
+++ network/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class UserFollowing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    following_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+
+
+class Posts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    time_posted = models.DateField(auto_now_add=True)
+    content = models.CharField(max_length=300)
+    like_count = models.IntegerField()
